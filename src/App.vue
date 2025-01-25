358b75c8bcdd60f903b8250f0e6ede1f0e25966e
hello <feoy1234@gmail.com>
2025-01-25 22:25:17 +0700

    initial project

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import { ref } from 'vue';
+  const cavasRef = ref(null)
+  const score = ref(0)
+</script>
+
+<template>
+  <section className="bg-base-100 max-w-screen-xl mx-auto py-14">
+      <div className="mx-auto flex flex-col items-center">
+        <div className=" "></div>
+        <h1 className="text-3xl mb-5">Score : {{ score }} </h1>
+
+        <canvas className="bg-blue-100 border-b-[15px] border-b-orange-950 "></canvas>
+
+        <button className="btn mt-4" onClick={resetGame}>
+          Reset Game
+        </button>
+      </div>
+    </section>
+</template>
+
+<style scoped>
+</style>
